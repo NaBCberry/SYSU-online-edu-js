d1678bce582c67495f8255cea8b6a2d9cc26583f
NaBCberry <66545291+NaBCberry@users.noreply.github.com>
2025-11-27 11:54:06 +0800

    修改检测时间

--- SYSU在线教学平台.js
+++ SYSU在线教学平台.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SYSU在线教学平台助手
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  中山大学在线教育平台心理线上课等课程自动跳转。等待页面加载完成后，首先点击播放按钮，检查目标值（播放进度），然后点击下一个视频按钮。若检测到“发表论坛帖子”则直接跳转到下一个活动
 // @author       蓝厘榨只因
 // @match        *://lms.sysu.edu.cn/mod/fsresource/*
@@ -10,7 +10,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     const targetValue = 100;  // 设置目标值为 100（例如：观看进度达到 100%）
